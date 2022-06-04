332fb8694206b3188c59d2e32e361b86774020db
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-04 20:55:59 +0530

    Update welcome.py

--- Innexia/modules/welcome.py
+++ Innexia/modules/welcome.py
@@ -1314,6 +1314,9 @@ dispatcher.add_handler(WELCOME_MUTE_HELP)
 dispatcher.add_handler(CAPTCHA_BUTTON_VERIFY_HANDLER)
 
 __mod_name__ = "Greetings"
+__button__ = ""
+__buttons__ = ""
+
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
