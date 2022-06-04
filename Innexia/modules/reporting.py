fff9c2b421fc998565d11d0085386a3b9cebe906
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-04 20:58:45 +0530

    Update reporting.py

--- Innexia/modules/reporting.py
+++ Innexia/modules/reporting.py
@@ -292,6 +292,8 @@ dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
 __mod_name__ = "Reporting"
+__button__ = ""
+__buttons__ = ""
 __handlers__ = [
     (REPORT_HANDLER, REPORT_GROUP),
     (ADMIN_REPORT_HANDLER, REPORT_GROUP),
