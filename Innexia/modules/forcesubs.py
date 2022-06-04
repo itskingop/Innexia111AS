9bc4562a6904a7058bea17a7be1abe209e9675f7
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-04 12:01:23 +0530

    Update forcesubs.py

--- Innexia/modules/forcesubs.py
+++ Innexia/modules/forcesubs.py
@@ -195,9 +195,7 @@ But then you have many groups, and you don't want this spammer to be in one of y
 *No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
 You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
 *Commands:*\n
-Feds are now divided into 3 sections for your ease.
-• `/fedownerhelp`*:* Provides help for fed creation and owner only commands
-• `/fedadminhelp`*:* Provides help for fed administration commands
-• `/feduserhelp`*:* Provides help for commands anyone can use
 """
-__mod_name__ = "F-Sub/Feds"
+__mod_name__ = "F-Sub"
+__button__ = ""
+__buttons__ = ""
