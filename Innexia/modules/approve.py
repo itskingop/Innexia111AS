85749f2878810cca4a8e07deb593418345396e5b
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-04 11:48:06 +0530

    Done

--- Innexia/modules/approve.py
+++ Innexia/modules/approve.py
@@ -223,5 +223,7 @@ dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
 __mod_name__ = "Approvals"
+__button__ = ""
+__buttons__ = ""
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
