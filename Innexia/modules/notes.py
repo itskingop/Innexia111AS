fec05ba14fa79ebbf84394ba77123858e0c1ec42
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-06-04 21:01:12 +0530

    CallBack error fixes 😗

--- Innexia/modules/notes.py
+++ Innexia/modules/notes.py
@@ -574,6 +574,8 @@ A button can be added to a note by using standard markdown link syntax - the lin
 """
 
 __mod_name__ = "Notes"
+__button__ = ""
+__buttons__ = ""
 
 GET_HANDLER = CommandHandler("get", cmd_get, run_async=True)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get, run_async=True)
